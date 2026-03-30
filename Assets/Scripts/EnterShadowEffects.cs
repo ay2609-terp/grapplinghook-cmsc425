d1092303c03e2595e5e42f3ef2df2e332d368156
Bao Nguyen <baolcaonguyen@gmail.com>
2026-03-30 15:24:43 -0400

    Add Darkness And Desaturation Effect On Enter Shadow

--- Assets/Scripts/EnterShadowEffects.cs
+++ Assets/Scripts/EnterShadowEffects.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class ShadowEffects : MonoBehaviour
 {
+    public GameObject CurrentVolume;
+    public float DesaturationFactor;
+    public float ExposureDecreaseFactor;
+
     private bool inShadow;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         
@@ -38,13 +44,54 @@ public class ShadowEffects : MonoBehaviour
     // Should use coroutine instead 
     void Update()
     {
+        Volume volume = CurrentVolume.transform.GetComponent<Volume>();
+        if (volume == null)
+        {
+            Debug.Log("CurrentVolume GameObject has no Volume component");
+            return;
+        }
+
+        if (volume.profile == null)
+        {
+            Debug.Log("Volume has no profile assigned");
+            return;
+        }
+
+        ColorAdjustments volumeColor;
+        if (!volume.profile.TryGet(out volumeColor))
+        {
+            Debug.Log("Volume profile has no ColorAdjustments override");
+            return;
+        }
+
         if (inShadow)
         {
-            
+
+            volumeColor.saturation.value -= DesaturationFactor * Time.deltaTime;
+            volumeColor.postExposure.value -= ExposureDecreaseFactor * Time.deltaTime;
         }
         else
         {
-            
+            float saturationChange = DesaturationFactor * Time.deltaTime * 2;
+            float exposureChange = ExposureDecreaseFactor * Time.deltaTime * 2; 
+
+            if (volumeColor.saturation.value + saturationChange > 0)
+            {
+                volumeColor.saturation.value = 0;
+            }
+            else
+            {
+                volumeColor.saturation.value += saturationChange;
+            }
+
+            if (volumeColor.postExposure.value + exposureChange > 0)
+            {
+                volumeColor.postExposure.value = 0;
+            }
+            else
+            {
+                volumeColor.postExposure.value += exposureChange;
+            }
         }
     }
 }
